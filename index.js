a4b8863b9e3ce940c3574c49b0907b19eb264327
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-10-04 15:07:15 -0600

    new JS functionality

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const headerChange = () => {
   (count%2 == 0) ? header.style.color = "black" : header.style.color = "red";
   count++;
 };
+
+// const changeTitle = () =>{
+//   let title = prompt("Enter new title")
+//   header.innerText = title;
+// };
+
+const changeTitle = () =>{
+  let title = header.innerText;
+  header.innerHTML = '<input class="titleInput" type="text" name="" value="" placeholder="' + title + '">';
+  header.firstChild.focus();
+};
+
+header.addEventListener("click", changeTitle);
