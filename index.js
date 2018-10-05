75f420e64522d517c5cbce12cec0a3ee8d32df29
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-10-04 19:03:11 -0600

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const header = document.querySelector("h1");
-let count = 1
+const list = document.querySelector("#list");
 
-const headerChange = () => {
-  (count%2 == 0) ? header.style.color = "black" : header.style.color = "red";
-  count++;
-};
-
-// const changeTitle = () =>{
-//   let title = prompt("Enter new title")
-//   header.innerText = title;
-// };
-
+// Function that allows the user to update the title of the list through a prompt.
 const changeTitle = () =>{
-  let title = header.innerText;
-  header.innerHTML = '<input class="titleInput" type="text" name="" value="" placeholder="' + title + '">';
-  header.firstChild.focus();
+  let title = prompt("Enter new title")
+  header.innerText = title;
 };
 
 header.addEventListener("click", changeTitle);
+
+const addItem = () => {
+  let itemValue = document.querySelector("#new").value;
+  let newItem = document.createElement("li");
+  newItem.appendChild(itemValue);
+  list.appendChild(newItem);
+  console.log(newItem);
+
+};
+
+
+
+// Function that changes the title by creating a input box.
+// const changeTitle = () =>{
+//   let title = header.innerText;
+//   header.innerHTML = '<input class="titleInput" type="text" name="" value="" placeholder="' + title + '">';
+//   header.firstChild.focus();
+// };
+
+// // Function that toggles the color of h1 back and forth.
+// let count = 1
+// const headerChange = () => {
+//   (count%2 == 0) ? header.style.color = "black" : header.style.color = "red";
+//   count++;
+// };
