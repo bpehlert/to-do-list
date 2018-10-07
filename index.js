f5e89c56d1d6d7903309e75ad21738c7e190f1b2
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-10-06 18:39:38 -0600

    Cleaned up JS

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ newItem.addEventListener("keypress", (event)=> {
   }
 });
 
-// Function that adds event listeners to all li elements
+// Adds event listeners to all li elements to toggle done class
 const addListeners = (item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("done");
   });
 };
 
-// Function to add delete buttons to each li elements
+// Adds delete buttons with "click" listeners to each li elements
 const addDeleteBtn = (item) => {
   let btn = document.createElement("button");
   btn.appendChild(document.createTextNode("x"));
@@ -51,15 +51,14 @@ const loopElements = () => {
   });
 };
 
-// Function that checks to see if there are any items on list
-const checkIfList = () => {
-  if (listItems.length === 0) {
-    let p = document.createElement('p');
-    p.appendChild(document.createTextNode("No items on your to do list."))
-    ul.appendChild(p);
-  }
-}
+// // Function that checks to see if there are any items on list
+// const checkIfList = () => {
+//   if (listItems.length === 0) {
+//     let p = document.createElement('p');
+//     p.appendChild(document.createTextNode("No items on your to do list."))
+//     ul.appendChild(p);
+//   }
+// }
+// checkIfList();
 
-
-checkIfList();
 loopElements();
