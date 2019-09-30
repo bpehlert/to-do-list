b3652149e2e912b063256803b50c0ce4ce94fc32
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-09-30 11:51:07 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const ul = document.querySelector("#toDoList");
-const addBtn = document.querySelector("#addBtn");
-const userInput = document.querySelector("#userInput");
+const ul = document.getElementById("toDoList");
+const addBtn = document.getElementById("addBtn");
+const userInput = document.getElementById("userInput");
 
-function onAddBtnClick(e) {
+function onAddBtnClick() {
   if (userInput.value.length > 0) createItem();
 }
 
@@ -44,45 +44,3 @@ function createItem() {
 addBtn.addEventListener("click", onAddBtnClick);
 ul.addEventListener("click", onItemClick);
 userInput.addEventListener("keypress", onKeypress);
-
-// const ul = document.querySelector("#toDoList");
-// const addBtn = document.querySelector("#addBtn");
-// const userInput = document.querySelector("#userInput");
-
-// function onKeypress(e) {
-//   if (userInput.value.length > 0 && e.keyCode === 13) createItem();
-// }
-
-// function onClick() {
-//   if (userInput.value.length > 0) createItem();
-// }
-
-// function toggleDone() {
-//   this.classList.toggle("done");
-// }
-
-// function deleteItem() {
-//   ul.removeChild(this.parentNode);
-// }
-
-// // Refactor with event delegation in mind: add click listener to ul,
-// // but add check to see if it is a click on button or li
-// function createItem() {
-//   const item = document.createElement("span");
-//   const li = document.createElement("li");
-//   const deleteBtn = document.createElement("button");
-
-//   li.appendChild(document.createTextNode(userInput.value));
-//   li.addEventListener("click", toggleDone);
-
-//   deleteBtn.appendChild(document.createTextNode("x"));
-//   deleteBtn.addEventListener("click", deleteItem);
-
-//   item.append(li, deleteBtn);
-//   ul.appendChild(item);
-
-//   userInput.value = "";
-// }
-
-// addBtn.addEventListener("click", onClick);
-// userInput.addEventListener("keypress", onKeypress);
